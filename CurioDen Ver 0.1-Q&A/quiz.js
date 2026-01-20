e80eec2c043f7da92661fe5ba23b7d548803d7f9
student-james-ih <james.ispas-hellyar@students.plymouth.ac.uk>
2026-01-20 20:21:44 +0000

    Add files via upload

---
+++ CurioDen Ver 0.1-Q&A/quiz.js
@@ -0,0 +1,6 @@
+// console.log("Helllo quiz");
+
+// Get all of the questions and the text in them
+const question =document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choiceText"));
+console.log(choices);
